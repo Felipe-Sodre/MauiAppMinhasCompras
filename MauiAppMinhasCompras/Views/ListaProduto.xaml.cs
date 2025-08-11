482fe9f523b46ffeb95029412de3ed9a4cfc000a
Felipe-Sodré <felipe.souldre.com@hotmail.com>
2025-08-10 22:21:43 -0300

    Ajustes para entrega da atividade da agenda 03

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -2,8 +2,24 @@ namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
-	public ListaProduto()
-	{
-		InitializeComponent();
-	}
+    public ListaProduto()
+    {
+        InitializeComponent();
+    }
+
+	//Conecção com o botão adicionar
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		//Caso haja erro
+		try
+		{		//Linha de código que abre outra pagina
+			Navigation.PushAsync(new Views.NovoProduto());
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+
+    }
+
 }
